18d6f3eb149cc59e1733221b280b6db554e67aaf
hansader <hansader42@gmail.com>
2025-03-08 15:51:58 +0530

    Add files via upload

---
+++ project-2/calcu.js
@@ -0,0 +1,61 @@
+//q-1
+console.log(5 + 3.0 / 2);
+
+//q-2
+console.log << (10 / 3 * 3.0);
+
+//q-3
+let x = 10;
+console.log(x++ * --x);
+
+//q-4
+console.log(10.0 / 0);
+
+//q-5
+console.log(5 == 5.0);
+
+//q-6
+console.log(3.14 + 2.86);
+
+//q-7
+console.log("Hello" == "Hello");
+
+//q-8
+console.log(10 /3.0 ==10 / 3);
+
+//q-9
+console.log(10 / 4 * 2 + 3 % 2);
+
+//q-10
+let a = 5, b = 2;
+console.log((a / b));
+
+//q-11
+console.log(String(10) + 5);
+
+//q-12
+console.log(5 < 10 < 0);
+
+//q-13
+console.log(Math.trunc(3.9) + 1);
+
+//q-14
+console.log((1 / 2.0) + (1 / 2.0));
+
+//q-15
+console.log((1.0 / 2) + (1 / 2));
+
+//q-16
+console.log((10 / 3.0) + (10 / 3));
+
+//q-17
+console.log(String(5) + String(5));
+
+//q-18
+console.log((Number(10 / 3) === (10 / 3.0)));
+
+//q-19
+console.log((10 / 3.0) * 3);
+
+//q-20
+console.log((Number(5 / 2) + (5 / 2.0)));
